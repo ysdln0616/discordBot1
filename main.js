fabc8e2d460f77aee3d0fabb85032aa403f49785
pnly <pnlybubbles@gmail.com>
2020-08-27 23:02:20 +0900

    バージョンを固定して、アップデート

--- main.js
+++ main.js
@@ -11,21 +11,21 @@ client.on('ready', message =>{
 });
 
 client.on('voiceStateUpdate', (oldGuildMember, newGuildMember) =>{
-  if(oldGuildMember.voiceChannelID !== undefined &&oldGuildMember.voiceChannelID !== null && newGuildMember.voiceChannelID !== undefined){
-    if(oldGuildMember.voiceChannelID == "745072327766704249") {
-      sendMsg(mainChannelId, "<@" + newGuildMember.user.id +"> が<#745072327766704249>を抜けました！\n");
-    }else if(oldGuildMember.voiceChannelID == "745055397328453667"){
-      sendMsg(mainChannelId, "<@" + newGuildMember.user.id +"> が<#745055397328453667>を抜けました！\n");
-    }  
+  if(oldGuildMember.channelID !== undefined &&oldGuildMember.channelID !== null && newGuildMember.channelID !== undefined){
+    if(oldGuildMember.channelID == "745072327766704249") {
+      sendMsg(mainChannelId, "<@" + newGuildMember.member.id +"> が <#745072327766704249> を抜けました！\n");
+    }else if(oldGuildMember.channelID == "745055397328453667"){
+      sendMsg(mainChannelId, "<@" + newGuildMember.member.id +"> が <#745055397328453667> を抜けました！\n");
+    }
   }
-  if(oldGuildMember.voiceChannelID === undefined ||oldGuildMember.voiceChannelID === null|| oldGuildMember.voiceChannelID === "745055397328453667"&& newGuildMember.voiceChannelID !== undefined){
-    if(newGuildMember.voiceChannelID == "745072327766704249") {
-        sendMsg(mainChannelId, "<@" + newGuildMember.user.id +"> が<#745072327766704249>で通話を開始しました！\n");
+  if(oldGuildMember.channelID === undefined ||oldGuildMember.channelID === null|| oldGuildMember.channelID === "745055397328453667"&& newGuildMember.channelID !== undefined){
+    if(newGuildMember.channelID == "745072327766704249") {
+        sendMsg(mainChannelId, "<@" + newGuildMember.member.id +"> が <#745072327766704249> で通話を開始しました！\n");
       }
   }
-  if(oldGuildMember.voiceChannelID === undefined ||oldGuildMember.voiceChannelID === null|| oldGuildMember.voiceChannelID === "745072327766704249"&& newGuildMember.voiceChannelID !== undefined){
-    if(newGuildMember.voiceChannelID == "745055397328453667") {
-      sendMsg(mainChannelId, "<@" + newGuildMember.user.id +"> が<#745055397328453667>で通話を開始しました！\n");
+  if(oldGuildMember.channelID === undefined ||oldGuildMember.channelID === null|| oldGuildMember.channelID === "745072327766704249"&& newGuildMember.channelID !== undefined){
+    if(newGuildMember.channelID == "745055397328453667") {
+      sendMsg(mainChannelId, "<@" + newGuildMember.member.id +"> が <#745055397328453667> で通話を開始しました！\n");
     }
   };
 });
@@ -39,7 +39,7 @@ if (process.env.DISCORD_BOT_TOKEN == undefined){
 client.login( process.env.DISCORD_BOT_TOKEN );
 
 function sendMsg(channelId, text){
-  client.channels.get(channelId).send(text)
+  client.channels.cache.get(channelId).send(text)
     .then(console.log("メッセージ送信: " + text))
     .catch(console.error);
 }
