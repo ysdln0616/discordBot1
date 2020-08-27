18652c0c883be611463405a8c4529e5a68548fe5
pnly <pnlybubbles@gmail.com>
2020-08-28 00:10:54 +0900

    いらないコードを消した

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-const http = require('http');
-const querystring = require('querystring');
-const crypto = require('crypto');
 const discord = require('discord.js');
 const client = new discord.Client();
 const mainChannelId = "745724910697054318";
-const server = require('./server.js');
+require('./server.js');
 
 client.on('ready', message =>{
   console.log('Bot準備完了');
@@ -15,7 +12,7 @@ client.on('voiceStateUpdate', (oldGuildMember, newGuildMember) =>{
     sendMsg(mainChannelId, "<@" + newGuildMember.member.id +"> が <#" + newGuildMember.channel.id + "> で通話を開始しました！");
   } else if (oldGuildMember.channel !== null && newGuildMember.channel === null) {
     sendMsg(mainChannelId, "<@" + oldGuildMember.member.id +"> が <#" + oldGuildMember.channel.id + "> を抜けました！");
-    }
+  }
 });
 
 
